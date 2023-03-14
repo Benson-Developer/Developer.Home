287f6456f0e45a11337623fd9a4a6d5bcf557636
benson.developer <benson.developer@gmail.com>
2023-03-14 17:46:16 +0800

    HelloSFWorld v2

--- force-app/main/default/lwc/helloSFWorld/helloSFWorld.js
+++ force-app/main/default/lwc/helloSFWorld/helloSFWorld.js
@@ -1,3 +1,15 @@
 import { LightningElement } from 'lwc';
 
-export default class HelloSFWorld extends LightningElement {}
+export default class HelloWorld extends LightningElement {
+    greeting = 'World';
+    myInput = 'World';
+
+    handleChange(event){
+        this.greeting = event.target.value;
+        this.myInput = this.greeting;
+    }
+
+    handleClick () {
+		this.greeting = this.myInput + ". It's a pleasure to meet you";
+	}
+}
